66218521e42ddd387e8f0dfa7f3721a8d416c2eb
esgalimov <esgalimov@gmail.com>
2020-12-31 02:27:01 +0300

    Стартовое меню

--- uroven_tren.py
+++ uroven_tren.py
@@ -1,18 +1,74 @@
 import pygame
+import sys
 from oop_maybe import Person, Log, Camera
 from constans import *
 
 
+# это лучше делать вне класса
+pygame.init()
+pygame.display.set_caption('Level')
+size = WIDTH, HEIGHT
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(size)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+# тестовое стартовое меню
+def start_window():
+    screen.fill((0, 0, 0))
+    color_play = (100, 255, 100)
+    color_quit = (100, 255, 100)
+    font = pygame.font.Font(None, 150)
+    text_play = font.render("Play", True, color_play)
+    text_quit = font.render("Quit", True, color_quit)
+    text_x = WIDTH // 2 - text_play.get_width() // 2
+    text_y_play = HEIGHT // 2 - text_play.get_height() // 2 - HEIGHT // 8
+    text_y_quit = HEIGHT // 2 - text_quit.get_height() // 2 + HEIGHT // 8
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEMOTION:
+                if text_x <= event.pos[0] <= text_x + text_play.get_width() and \
+                        text_y_play <= event.pos[1] <= text_y_play + text_play.get_height():
+                    color_play = (50, 125, 50)
+                else:
+                    color_play = (100, 255, 100)
+                if text_x <= event.pos[0] <= text_x + text_play.get_width() and \
+                        text_y_quit <= event.pos[1] <= text_y_quit + text_quit.get_height():
+                    color_quit = (50, 125, 50)
+                else:
+                    color_quit = (100, 255, 100)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if text_x <= event.pos[0] <= text_x + text_play.get_width() and\
+                        text_y_play <= event.pos[1] <= text_y_play + text_play.get_height():
+                    return
+                if text_x <= event.pos[0] <= text_x + text_play.get_width() and \
+                        text_y_quit <= event.pos[1] <= text_y_quit + text_quit.get_height():
+                    terminate()
+
+        text_play = font.render("Play", True, color_play)
+        text_quit = font.render("Quit", True, color_quit)
+        screen.blit(text_play, (text_x, text_y_play))
+        screen.blit(text_quit, (text_x, text_y_quit))
+        clock.tick(FPS)
+        pygame.display.flip()
+
+
+start_window()
+
+
 class Level:
     def __init__(self, pers_x, pers_y, end_of_level, logs=[], enemies=[], do_center=True):
-        pygame.init()
-        pygame.display.set_caption('Level')
-        size = WIDTH, HEIGHT
         self.end_of_level = end_of_level
         self.do_center = do_center
-        self.screen = pygame.display.set_mode(size)
-        self.screen.fill((0, 0, 0))
-        self.clock = pygame.time.Clock()
+        screen.fill((0, 0, 0))
         self.running = True
         self.alp = 0
         self.pers = Person(pers_x, pers_y)
@@ -24,7 +80,7 @@ class Level:
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.stop_drun()
+                    self.stop_run()
                 if event.type == pygame.KEYDOWN:
                     # d down
                     if event.key == pygame.K_d:
@@ -41,7 +97,7 @@ class Level:
                     # esc - остановка уровня
                     if event.key == pygame.K_ESCAPE:
                         self.stop_run()
-                    self.clock.tick()
+                    clock.tick()
                 if event.type == pygame.KEYUP:
                     # d up
                     if event.key == pygame.K_d:
@@ -62,8 +118,8 @@ class Level:
             if WIDTH // 2 < self.pers.rect.x - self.logs[0].rect.x < self.end_of_level - WIDTH // 2:
                 self.camera.cam_on = True
 
-            self.screen.fill((0, 0, 0))
-            self.clock.tick(FPS)
+            screen.fill((0, 0, 0))
+            clock.tick(FPS)
             self.pers.run()
             self.pers.fly()
             self.drawing()
@@ -74,7 +130,7 @@ class Level:
         self.camera.update(self.pers)
         for sprite in all_sprites:
             self.camera.apply(sprite)
-        all_sprites.draw(self.screen)
+        all_sprites.draw(screen)
 
     def stop_run(self):
         font1 = pygame.font.Font(None, 50 * int(HEIGHT * WIDTH / (1366 * 768)))
@@ -91,7 +147,7 @@ class Level:
         check_screen.fill((0, 0, 0))
         check_screen.blit(text1, (max(w1, w2) - w1, 0))
         check_screen.blit(text2, (max(w1, w2) - w2, h1))
-        self.screen.blit(check_screen, (WIDTH // 2 - max(w1, w2) // 2, HEIGHT // 2 - (h1 + h2) // 2))
+        screen.blit(check_screen, (WIDTH // 2 - max(w1, w2) // 2, HEIGHT // 2 - (h1 + h2) // 2))
         pygame.display.flip()
         to_run = True
         error_stop = False
